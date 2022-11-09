9264437af67041d72abaeb9f3a5432e843ae61c7
eq-hong <111675243+eq-hong@users.noreply.github.com>
2022-11-10 00:35:59 +1300

    update Blathers.jsx with function parameters

--- client/components/Blathers.jsx
+++ client/components/Blathers.jsx
@@ -3,30 +3,40 @@ import request from 'superagent'
 
 import Spinner from './Spinner'
 
+const initialData='fish'
+const dataTypes=['fish','sea','bugs','fossils']
+
+const idTest=1
+
 function Blathers() {
-  const [bugData, setBugData] = useState(null)
-  const [bugID, setBugID] = useState('')
+  const [apiData, setApiData] = useState(null)
+  const [apiID, setApiID] = useState('')
   const [isError, setIsError] = useState(false)
 
-  useEffect(() => {
+  function typeOfData(data, id) {
+    useEffect(() => {
     request
-      .get(`https://acnhapi.com/v1/bugs/1`)
-      .then ((response) => {
-        setBugData(response.body)
-        console.log(response.body)
-        setIsError(false)
-      })
-      .catch(() => {
-        setIsError(true)
-  })
-}, [])
+    .get(`https://acnhapi.com/v1/${data}/${id}`)
+    .then ((response) => {
+      setApiData(response.body)
+      console.log(response.body)
+      setIsError(false)
+    })
+    .catch(() => {
+      setIsError(true)
+    })
+   }, [])
+  }
 
-if (isError) return <h1>{"Oops can't fetch 'em all today!"}</h1>
+  typeOfData(initialData, idTest)
+
+
+  if (isError) return <h1>{"Oops can't fetch 'em all today!"}</h1>
 
   return(
     <section>
     <h3>Museum Text:</h3>
-    <h4>{bugData?.['museum-phrase']}</h4>
+    <h4>{apiData?.['museum-phrase']}</h4>
     </section>
   )
 }
