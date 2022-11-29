5e773d097332a529480ea1c8f25e70f8beb2b445
eq-hong <111675243+eq-hong@users.noreply.github.com>
2022-11-29 23:37:17 +1300

    Update sort, filter and map

--- client/components/Calculation.jsx
+++ client/components/Calculation.jsx
@@ -3,15 +3,13 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { fetchRooms, updateTotalArea } from '../actions'
-import { getAllRooms } from '../apiClient'
 
 function Calculation(){
-  // const [rooms, setRooms] = useState(null)
   const rooms = useSelector((state) => state.rooms)
   const totalArea = useSelector((state) => state.totalArea)
   const dispatch = useDispatch()
   const [input, setInput] = useState('')
-  const [buildCostSqm, setBuildCostSqm] = useState(3500)
+  const [buildCostSqm, setBuildCostSqm] = useState(4000)
 
   const style = {
     marginTop: `2.5%`,
@@ -37,23 +35,72 @@ function Calculation(){
     setBuildCostSqm(input)
   }
 
-  const costEstimatePerSqm = 3500
+  function sortByPriority() {
+    // https://stackoverflow.com/questions/65687818/js-sort-array-of-objets-by-specific-string-property-not-ascending-or-descending
+    const sortingScheme = [
+      "High",
+      "Mid",
+      "Low",
+      ""
+    ]
+    return function(a, b) {
+      const indexOfa = sortingScheme.indexOf(a.priority);
+      const indexOfb = sortingScheme.indexOf(b.priority);
+      if (indexOfa > indexOfb)
+        return 1
+      if (indexOfa < indexOfb)
+        return -1
+      return 0;
+    }
+  }
+
   const buildCostEstimate = new Intl.NumberFormat('en-NZ', { style: 'currency', currency: 'NZD' }).format(totalArea*buildCostSqm)
+  const roomsCopy = [...rooms]
+  const roomsSorted = roomsCopy?.sort(sortByPriority())
+
+  function filterItems(array, query, value) {
+    // console.log(array, 'array');
+    // console.log(value, 'value');
+    return array.filter( ( {roomType} ) => roomType.toLowerCase().includes(query.toLowerCase()));
+  }
+
+  function mapRoom(roomtype) {
+    const filteredRoom = filterItems(roomsSorted, roomtype)
+   return filteredRoom?.map((oneRoom) => {
+      const { id, roomName, roomType, priority, width, length, } = oneRoom
+      const roomArea = (width)*(length)
+      return (
+        <div key={id}>
+          {roomArea}sqm ({roomName}, {priority}) 
+        </div>
+      )
+      })
+  }
 
   return(
     <section style={style} className="container">
     <h4>Calculation</h4>
     
-    {rooms && rooms?.map((oneRoom) => {
-      const { id, roomName, width, length, } = oneRoom
-      const roomArea = (width)*(length)
-      return (
-        <div key={id}>
-          {roomArea}sqm ({roomName})
-        </div>
-      )
-      })
-    }
+    <b>Living</b><br></br>
+    {mapRoom('Living')}
+    <p></p>
+    
+    <b>Kitchen</b><br></br>
+    {mapRoom('kitchen')}
+    <p></p>
+
+    <b>Bedroom</b><br></br>
+    {mapRoom('Bedroom')}
+    <p></p>
+
+    <b>Bathroom</b><br></br>
+    {mapRoom('Bathroom')}
+    <p></p>
+
+    <b>Other</b><br></br>
+    {mapRoom('Other')}
+    <p></p>
+
     <br></br>
     <h5>Total area | {totalArea}sqm</h5>
     Build cost per sqm = {new Intl.NumberFormat('en-NZ', { style: 'currency', currency: 'NZD' }).format(buildCostSqm)}
@@ -62,7 +109,7 @@ function Calculation(){
     <p></p>
     <form>
     Update build cost per sqm <br></br> <input name="input" onChange={handleChangeBuildCostSqm} placeholder={buildCostSqm}/> 
-    <br></br><button onClick={handleUpdateBuildCostSqm}>Update</button>
+    <br></br><button onClick={handleUpdateBuildCostSqm}>Update Build Cost</button>
     </form>
     
     <div className='add-button'>
@@ -83,7 +130,21 @@ export default Calculation
         
 // const { id, roomName, roomType, roomNotes, priority, width, length, north, east, west, south, floor } = oneRoom
 
-// dispatch(setRoomArea(oneRoom.id, roomArea))
-//   console.log('oneRoom.id', oneRoom.id,);
-//   console.log('roomArea', roomArea)
-//   console.log('setRoomArea', setRoomArea())
+
+  // // https://stackoverflow.com/questions/979256/sorting-an-array-of-objects-by-property-values
+  // const sort_by = (field, reverse, primer) => {
+
+  //   const key = primer ?
+  //     function(x) {
+  //       return primer(x[field])
+  //     } :
+  //     function(x) {
+  //       return x[field]
+  //     };
+  
+  //   reverse = !reverse ? 1 : -1;
+  
+  //   return function(a, b) {
+  //     return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+  //   }
+  // }
--- client/reducers/rooms.js
+++ client/reducers/rooms.js
@@ -6,7 +6,7 @@ const roomsReducer = (state = initialRoomState, action) => {
     case 'SET_ROOMS':
       return payload
     case 'DEL_ROOM':
-      return state.filter((room) => room.id !== payload)
+      return state.filter((room) => room.id !== payload.id)
     case 'UPDATE_ROOM': 
       return state.map((room) => room.id == payload.id ? payload.updatedRoom : room)
     case 'ADD_ROOM':
